a34bcd6a2ed7710241fbca17162f36c707b98935
loretodev <loreto.galvez.e@gmail.com>
2023-05-07 12:34:30 -0400

    Objeto que conecta a la BDatos y arreglo en menu.

---
+++ src/main/java/utils/DatabaseConnection.java
@@ -0,0 +1,16 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static String URL = "jbdc:mysql://localhost:3306/javaweb?serverTimezone=America/Santiago";
+    private static String USERNAME = "root";
+    private static String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        //return DriverManager.getConnection(url, username, password);
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/javaweb?serverTimezone=America/Santiago", "root", "");
+    }
+}
