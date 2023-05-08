4b3ec4342085f3c62612c855b8954837aadb0ddb
Carolina Ceron <kro.ceron@gmail.com>
2023-05-08 17:03:56 -0400

    soluciones temporales

---
+++ src/main/java/controlador/LoginServlet.java
@@ -0,0 +1,80 @@
+package controlador;
+import implementacion.LoginServiceImpl;
+import interfaces.*;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import java.util.Optional;
+
+public class LoginServlet extends HttpServlet {
+    private Connection conexion;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        // Establecer la conexión a la base de datos en el método init()
+        String url = "jdbc:mysql://localhost:3306/sprint_m5";
+        String usuario = "usuario";
+        String contrasena = "contrasena";
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conexion = DriverManager.getConnection(url, usuario, contrasena);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        LoginInterface auth = new LoginServiceImpl();
+        Optional<String> usernameOptional = auth.getUsername(req);
+
+        if (usernameOptional.isPresent()) {
+            resp.setContentType("text/html;charset=UTF-8");
+            try (PrintWriter out = resp.getWriter()) {
+
+                out.println("<!DOCTYPE html>");
+                out.println("<html>");
+                out.println("    <head>");
+                out.println("        <meta charset=\"UTF-8\">");
+                out.println("        <title>Hola " + usernameOptional.get() + "</title>");
+                out.println("    </head>");
+                out.println("    <body>");
+                out.println("        <h1>Hola " + usernameOptional.get() + " ya has iniciado sesión con éxito!</h1>");
+                out.println("        <p><a href='" + req.getContextPath() + "/index.html'>Volver</a></p>");
+                out.println("        <p><a href='" + req.getContextPath() + "/logout'>Logout</a></p>");
+                out.println("    </body>");
+                out.println("</html>");
+            }
+        } else {
+            getServletContext().getRequestDispatcher("/login.html").forward(req, resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        if (username.equals(username) && password.equals(password)) {
+
+            HttpSession session = req.getSession();
+            session.setAttribute("username", username);
+
+            resp.sendRedirect(req.getContextPath() + "/login");
+        } else {
+            resp.sendRedirect(req.getContextPath() + "/login");
+            //resp.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Lo sentimos no esta autorizado para ingresar a esta pagina!");
+        }
+    }
+}
---
+++ src/main/java/implementacion/LoginImpl.java
@@ -0,0 +1,23 @@
+package implementacion;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import controlador.ConexionBD_Login;
+public class LoginImpl {
+    public static void main(String[] args) throws SQLException, SQLException {
+        String url = "jdbc:mysql://localhost:3306/sprint_m5";
+        String usuario = "nombre_de_usuario";
+        String contrasena = "contraseña_de_usuario";
+
+        ConexionBD_Login conexion = new ConexionBD_Login(url, usuario, contrasena);
+
+        ResultSet rs = conexion.ejecutarConsulta("SELECT nombre_usuario FROM usuarios");
+
+        while (rs.next()) {
+            String nombreUsuario = rs.getString("nombre_usuario");
+            System.out.println(nombreUsuario);
+        }
+
+        conexion.cerrarConexion();
+    }
+}
